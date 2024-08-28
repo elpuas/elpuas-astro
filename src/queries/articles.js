4bc3fd49ad28a079ffa71cb6c0e0ec767214e5b0
Alfredo Navas <elpuas@gmail.com>
2024-08-28 17:37:32 -0600

    On branch feature/blog 	modified:   src/components/BaseHead.astro 	modified:   src/layouts/BaseLayout.astro 	modified:   src/pages/blog/[...slug].astro 	new file:   src/pages/lets-talk.astro 	new file:   src/pages/what-i-do.astro 	new file:   src/queries/articles.js

---
+++ src/queries/articles.js
@@ -0,0 +1,63 @@
+export const ARTICLES_QUERY = `
+    query AllArticles {
+        allArticles {
+            categories {
+            title
+            slug
+            _modelApiKey
+            }
+            featuredImage {
+            responsiveImage {
+                src
+                height
+                width
+            }
+            title
+            }
+            content {
+            ... on BlockCodeRecord {
+                id
+                _modelApiKey
+                code {
+                value
+                }
+            }
+            ... on BlockParagraphRecord {
+                id
+                _modelApiKey
+                content {
+                value
+                }
+            }
+            ... on BlockContentWithImageRecord {
+                id
+            }
+            ... on BlockGalleryRecord {
+                id
+                images {
+                responsiveImage {
+                    src
+                    width
+                    height
+                }
+                }
+                galleryStyle
+                _modelApiKey
+            }
+            ... on BlockImageRecord {
+                id
+                alignment
+                _modelApiKey
+            }
+            }
+            articleAuthor
+            articleDisplayDate
+            _seoMetaTags {
+            attributes
+            content
+            }
+            slug
+            title
+        }
+    }
+`;
